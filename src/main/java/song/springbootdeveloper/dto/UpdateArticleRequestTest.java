832f611793f9a32053286192393872ddfe2ef948
DoNalD-A <thdlstjrj@naver.com>
2025-04-05 20:59:43 +0900

    UpdateArticleuserTest

---
+++ src/main/java/song/springbootdeveloper/dto/UpdateArticleRequestTest.java
@@ -0,0 +1,13 @@
+package song.springbootdeveloper.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class UpdateArticleRequestTest {
+    private String title;
+    private String content;
+}
