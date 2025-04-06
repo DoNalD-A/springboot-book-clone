c12295951250b5eb898157139581f92347b279e8
DoNalD-A <thdlstjrj@naver.com>
2025-04-06 20:34:33 +0900

    ArticleViewResponseTest add

---
+++ src/main/java/song/springbootdeveloper/dto/ArticleViewResponseTest.java
@@ -0,0 +1,24 @@
+package song.springbootdeveloper.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import song.springbootdeveloper.domain.Article;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@Getter
+public class ArticleViewResponseTest {
+
+    private Long id;
+    private String title;
+    private String content;
+    private LocalDateTime createdAt;
+
+    public ArticleViewResponse(Article article) {
+        this.id = article.getId();
+        this.title = article.getTitle();
+        this.content = article.getContent();
+        this.createdAt = article.getCreatedAt();
+    }
+}
